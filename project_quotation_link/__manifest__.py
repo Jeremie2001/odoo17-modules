e9321a4b00e294cf357fc4f13e22679a3bdff822
Jeremie2001 <ndjolijeremie@gmail.com>
2026-09-02 12:47:49 +0000

    update  module project quotation link1 17.0 banner3

--- project_quotation_link/__manifest__.py
+++ project_quotation_link/__manifest__.py
@@ -27,6 +27,7 @@ Ideal for companies managing several projects for the same client.
     
 
     'images': [
+        'static/description/banner.png',
         'static/description/icon.png',
         'static/description/01_creation_projet.png',
         'static/description/02_projet_avec_devis.png',
