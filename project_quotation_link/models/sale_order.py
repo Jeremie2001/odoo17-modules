1786bc6d602ff37df1e9394899a507da8ae3a761
Jeremie2001 <ndjolijeremie@gmail.com>
2026-09-02 12:20:30 +0000

    add new module project quotation link 19.0

---
+++ project_quotation_link/models/sale_order.py
@@ -0,0 +1,12 @@
+from odoo import models, fields, api
+
+class SaleOrderInherit(models.Model):
+    _inherit = "sale.order"
+
+    projet = fields.Many2one(
+        comodel_name='project.project',
+        string="Projet",
+        index=True,
+        store=True,
+        readonly=False,
+    )
